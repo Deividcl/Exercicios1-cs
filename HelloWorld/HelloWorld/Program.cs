75de6455ddca1366534625114c6e8fe975331212
Deivid Cesar <36942232+Deividcl@users.noreply.github.com>
2018-08-28 17:39:28 -0300

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nQual a sua idade? ");
+            var age = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nOlá, tenho {age}, em {date:d} as {date:t}!");
+            Console.Write("\nClique para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
